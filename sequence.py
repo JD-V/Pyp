87dc0fdf3fdabfe1991317ebc20fd28d9f19bfd5
JD-V <vaghasiyajaydeep@gmail.com>
2018-08-16 23:48:44 +0530

    updated soution

--- sequence.py
+++ sequence.py
@@ -1,4 +1,7 @@
 print("enter number :")
 N= int(input())
-for i in range(1,N+1):
-    print ('%s'% i*i)
+for i in range(1,N):
+    print ( (i*10**(i-9) if (i-9)>=0 else 0) +        (i*10**(i-8) if (i-8)>=0 else 0) +         (i*10**(i-7) if (i-7)>=0 else 0) +         (i*10**(i-6) if (i-6)>=0 else 0) +         (i*10**(i-5) if (i-5)>=0 else 0) +       (i*10**(i-4) if (i-4)>=0 else 0) +         (i*10**(i-3) if (i-3)>=0 else 0) +         (i*10**(i-2) if (i-2)>=0 else 0) +        (i*10**(i-1) if (i-1)>=0 else 0 )        )
+
+
+        
